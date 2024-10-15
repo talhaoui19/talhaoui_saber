f7ceb3f31c45d078a14f7de5604027f1f9fd25a3
saber <talhaoui.sabir@gmail.com>
2024-10-15 22:00:32 +0200

    edit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -197,7 +197,73 @@ export default function Header() {
                 <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" />
               </svg>
             ) : (
-              (<span className="text-base font-bold">X</span>)
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+                version="1.1"
+                fill="#4b4b4b"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <title>Close</title>{" "}
+                  <g
+                    id="Page-1"
+                    stroke="none"
+                    stroke-width="1"
+                    fill="none"
+                    fill-rule="evenodd"
+                  >
+                    {" "}
+                    <g id="Close">
+                      {" "}
+                      <rect
+                        id="Rectangle"
+                        fill-rule="nonzero"
+                        x="0"
+                        y="0"
+                        width="24"
+                        height="24"
+                      >
+                        {" "}
+                      </rect>{" "}
+                      <line
+                        x1="16.9999"
+                        y1="7"
+                        x2="7.00001"
+                        y2="16.9999"
+                        id="Path"
+                        stroke="#4b4b4b"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                      >
+                        {" "}
+                      </line>{" "}
+                      <line
+                        x1="7.00006"
+                        y1="7"
+                        x2="17"
+                        y2="16.9999"
+                        id="Path"
+                        stroke="#4b4b4b"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                      >
+                        {" "}
+                      </line>{" "}
+                    </g>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
             )}
           </button>
 
