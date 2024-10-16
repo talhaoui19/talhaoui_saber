6e10cae2002597f5c8b5068f86342065f24dceb5
modecode22 <epiccoder00@gmail.com>
2024-10-16 21:08:49 +0100

    added

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -6,13 +6,15 @@ export async function GET() {
 
   try {
     const posts = await PostModal.find({}).sort({ createdAt: -1 });
+    console.log(posts, 'this is from the get handler');
+    
     return new Response(JSON.stringify(posts), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
     console.error("Error:", error);
-    return new Response(JSON.stringify({ message: "Failed to fetch posts" }), {
+    return new Response(JSON.stringify({ message: "Failed to fetch posts"+error }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
